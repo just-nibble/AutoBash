78f8e81b28258cc7c51f7a05982592e4c954b0a4
Princewill Olaiya <princewillolaiya@gmail.com>
2022-08-08 22:02:48 +0100

    added basic arch packages

---
+++ Arch/main.sh
@@ -0,0 +1,29 @@
+## Official Repos ##
+
+sudo pacman -Syu base-devel --no-confirm &&
+sudo pacman -Syu docker --no-confirm &&
+sudo pacman -Syu httrack --no-confirm &&
+# KVM
+sudo pacman -Syu archlinux-keyring --no-confirm &&
+sudo pacman -Syu quemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat --no-confirm &&
+sudo pacman -Syu ebtables iptables --no-confirm &&
+sudo pacman -Syu elibquestfs --no-confirm &&
+sudo pacman -Syu lutris --no-confirm &&
+sudo pacman -Syu steam --no-confirm &&
+sudo pacman -Syu telegram-desktop --no-confirm &&
+sudo pacman -Syu wine --no-confirm &&
+
+## aur ##
+sudo pamac build spotify --no-confirm && 
+
+## flatpaks ##
+flatpak install com.bitwarden.desktop -y &&
+
+## snaps ##
+onlyoffice-desktopeditors
+code --classic
+
+## Latte ##
+git clone https://github.com/tobecci/kde-settings/ &&
+qdbus org.kde.lattedock /Latte org.kde.LatteDock.switchToLayout "kde-settings/tobecci.layout.latte"
+
