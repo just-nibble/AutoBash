fd5c27927e771a675277b4ed4c884085adbf89de
Princewill Olaiya <princewillolaiya@gmail.com>
2022-08-09 16:50:15 +0100

    added auto response

--- Fedora/main.sh
+++ Fedora/main.sh
@@ -1,16 +1,16 @@
 ## Official Repos ##
-sudo dnf group install "C Development Tools and Libraries" "Development Tools"
-sudo dnf install cmake
+sudo dnf group install "C Development Tools and Libraries" "Development Tools" -y &&
+sudo dnf install cmake -y &&
 
 sudo dnf install dnf-plugins-core -y &&
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
-sudo dnf install docker-ce docker-ce-cli containerd.io &&
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo -y &&
+sudo dnf install docker-ce docker-ce-cli containerd.io -y &&
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm &&
-sudo dnf install telegram && 
-sudo dnf -y install bridge-utils libvirt virt-install qemu-kvm &&
-sudo dnf install libvirt-devel virt-top libguestfs-tools guestfs-tools &&
-sudo dnf -y install virt-manager &&
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y &&
+sudo dnf install telegram -y && 
+sudo dnf -y install bridge-utils libvirt virt-install qemu-kvm -y &&
+sudo dnf install libvirt-devel virt-top libguestfs-tools guestfs-tools -y &&
+sudo dnf -y install virt-manager -y &&
 
 ## Configure Docker ##
 sudo systemctl start docker.service &&
